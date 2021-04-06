9e34e4ac523f33b1f4cfdc981c818570efca8bc1
DociIva <doci.iva@hotmail.com>
2021-04-06 19:02:43 +0200

    add prima parte del loop

--- js/script.js
+++ js/script.js
@@ -40,27 +40,74 @@ var listaBombe = [];
 // numeri consentiti di tentativi per il giocatore
 var numeriConsentiti = [];
 
-// variabile per l'utente per ora = a 0 
+// variabile per la SCELTA DELL'UTENTE
 var utente = 0;
 
 // Genero le bombe ( fatti con 16 numeri casuali ma che sono Univoci però)
 // meglio usare while 
 while(listaBombe.length < numeroBombe){
-  var bomba = 34; // <-- da togliere poi il numero messo 
-
+  var bomba = numeroRandom(numeroMassimo); // prima inseriamo un numero es --> (34); poi è <-- da togliere poi il numero messo 
+                                          // è mettiamo numeroRandom(con il NUMEROMASSIMO )
   // decisione --> ! come === false
   if(! listaBombe.includes(bomba)) {
    listaBombe.push(bomba);
   }
 }
+// questo è per creare una collezione di Array di numero Univoci
+
 // per controllare
-console.table("Liste bombe ", listaBombe);
+console.table("Liste bombe ", listaBombe);   // <-- lo vede male perchè c'è più di un paramentro .
 
 
+/*****************************
+ * Gioco 
+ * Loop principale del gioco 
+ * 
+ * 
+ * ( ora chiediamo all'untente i numeri "lo facciamo diventare interativo anche per lui");
+ * - lui\lei mette un numero fino a 100, 'noi verifichiamo se è un numero delle bombe, oppure no';
+ * - se non è nei numeri delle bombe lo mettiamo nei numeriConsentiti, poi così chiediamo un altro numero;
+ * - controlliamo anche se è un numero che ci ha già inseritio prima, (così non può fare la ripetizione)
+ * - (chiedi in numero, verifica, e verifica dell'iterazione);
+ * - con due possibilità alla 'fine' (se ci arriva), o ha beccato la bomba, oppure ha finito le possibilità e così facendo ha vinto; 
+ * ********************************************** */
 
-/*********
- * Funzione utility
+/** perchè un while e non un for? N.B. --> perchè non abbiamo un numero prestabilito di volte per chiedere all'utente un numero
+ dipende da se trova una bomba oppure no , quidni abbiamo bisogno di un loop che vada in modo più flessibile
  */
+
+// desc. le condizioni, due componenti impo, 1- se siamo sotto il numero delle possibilità ( siamo sotto il nuemro delle posibilità? se si allora possimao entrare)
+// 2 - la scelta attuale dell'utente --> e se si, la scelta non è una bomba
+
+// come capire se siamo sotto il numero delle possibilità? num.Consentiti.length | su cosa basiamo il far? sul numero delle possibilità
+// rapporto che diamo a questi due è? < perche devo andare fin che la lente non è uguale lui deve andare (se è = hai vinto il gioco) (includes -> per cercare al suo interno)
+
+// Le due condizione per stare nel loop di gioco 1 vito ed esco, 2 ho preso perchè ho preso una bomba
+while((numeriConsentiti.length < possibilità) && (! listaBombe.includes(utente)) ) {
+  // scelta utente di inserimento, (sovrascriviamo la varibile dell'utente)
+  utente = parseInt(prompt("Inserisci per favore un numero da 1 a " + numeroMassimo + '\nTentativi riusciti ' + numeriConsentiti.length + 'di ' + possibilità));
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/****************************************************************
+ * Funzione utility
+ ****************************************************************/
 function nuemroRandom(max){
   return Math.floor(Math.random() * max ) + 1;
 }
